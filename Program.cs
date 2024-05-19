f648d2d198585b3a4ef4fd1a54da1cdbae0d161d
Matheus-Moura26 <moura.matheus26@gmail.com>
2024-05-19 17:32:44 -0300

    Atualização de SDK

--- Program.cs
+++ Program.cs
@@ -6,95 +6,92 @@ namespace Projeto_de_aula
     {
         static void Main(string[] args)
         {
-           
+           Console.WriteLine("Hello World");
         }
 
-            
-    }
-
-
-    public class EnemyAI : MonoBehavior
-    {
-        /* tipos de dados */
-        int valor = 30;
-        float a1 = 3.5f;
-        string catacteres = "Nome do personagem";
-        bool VerdadeiroOuFalso = false;
-
-        /* usados no exemplo */
-        int speed;
-        string enemyName;
-        float health;
-
-        /* array / lista*/
-        int [] intArray = new int[] {1,2,3,4,5,6,7,8,9,10};
-
-        List<int> intList = new List<int>() {1,2,3,4,5,6,7,8,9,10};
-
-
-        void Movement()
+/*              public class EnemyAI : MonoBehavior
         {
-            /* Alterando arrays e listas  */
-            intList.Add(11);
-            intList.Remove(11);
+            //tipos de dados 
+            int valor = 30;
+            float a1 = 3.5f;
+            string catacteres = "Nome do personagem";
+            bool VerdadeiroOuFalso = false;
 
-            /* leitura de arrays e listas */
-            foreach (int intNumbers in intArray)
-            {
-                
-            }
+            // usados no exemplo 
+            int speed;
+            string enemyName;
+            float health;
 
-            for (int i = 0; i < intArray.length; i++)
-            {
-                int currentNumber = intArray[i]
-            }
-            /* específicade de listas em for */
-            for (int i = 0; i < intList.count; i++)
-            {
-                int currentNumber = intList[i]
-            }
+            //array / lista
+            int [] intArray = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+            List<int> intList = new List<int>() {1,2,3,4,5,6,7,8,9,10};
 
 
-            if (speed >= 0 && health > 0)
+            void Movement()
             {
-                
-            }
+                // Alterando arrays e listas 
+                intList.Add(11);
+                intList.Remove(11);
 
-            void Dead()
-            {
-                if(health <= 0)
+                //leitura de arrays e listas 
+                foreach (int intNumbers in intArray)
                 {
+                    
+                }
 
+                for (int i = 0; i < intArray.length; i++)
+                {
+                    int currentNumber = intArray[i]
+                }
+                // específicade de listas em for 
+                for (int i = 0; i < intList.count; i++)
+                {
+                    int currentNumber = intList[i]
+                }
+
+
+                if (speed >= 0 && health > 0)
+                {
+                    
+                }
+
+                void Dead()
+                {
+                    if(health <= 0)
+                    {
+
+                    }
+                }
+
+                void Attack()
+                {
+                    EnemyWeapon.damage= 30;
+                    EnemyWeapon.firerate = 0.4f;
+                    EnemyWeapon.Shoot();
                 }
             }
+        }
 
-            void Attack()
+
+        public class EnemyWeapon
+
+        {
+            public float damage;
+            public float firerate;
+
+            public void Shoot()
             {
-                EnemyWeapon.damage= 30;
-                EnemyWeapon.firerate = 0.4f;
-                EnemyWeapon.Shoot();
+
             }
         }
-    }
+
+        void Explosao(int ValorDano){
+            //Tocar efeito sonoro
+            //Aplicar efeito de flash
+            //Aplicar ValorDano de dano na vida do personagem
 
 
-    public class EnemyWeapon
-
-    {
-        public float damage;
-        public float firerate;
-
-        public void Shoot()
-        {
-
-        }
-    }
-
-    void Explosao(int ValorDano){
-        //Tocar efeito sonoro
-        //Aplicar efeito de flash
-        //Aplicar ValorDano de dano na vida do personagem
-
-
-    }
+        } */
+    }   
 }
